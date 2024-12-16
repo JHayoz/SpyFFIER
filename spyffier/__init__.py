9ba6e689ea75742aa5db5d1bb49091409f74ae3d
JHayoz <jeanhayoz94@gmail.com>
2024-12-16 09:11:19 +0100

    Added init file and renamed main to core.py

---
+++ spyffier/__init__.py
@@ -0,0 +1,9 @@
+from spyffier.core import Pipeline
+from spyffier.utils import *
+
+__author__ = 'Jean Hayoz'
+__licence__ = 'MIT'
+__version__ = '0.1.0'
+__maintainer__ = 'Jean Hayoz'
+__email__ = 'jhayoz@phys.ethz.ch'
+__status__ = 'Development'
