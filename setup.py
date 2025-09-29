d84c710f32f73b80ad5d26c06f70487e4bf153c3
JHayoz <77056052+JHayoz@users.noreply.github.com>
2025-09-29 17:38:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,24 @@
-#!/usr/bin/env python
+from distutils.core import setup
 
-import pkg_resources
-import setuptools
-
-with open('requirements.txt') as req_txt:
-    parse_req = pkg_resources.parse_requirements(req_txt)
-    install_requires = [str(req) for req in parse_req]
-
-setuptools.setup(
+setup(
     name='spyffier',
     version='0.1.0',
-    description='Data reduction pipeline for VLT/ERIS/SPIFFIER',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/x-rst',
-    author='Jean Hayoz',
-    author_email='jhayoz@phys.ethz.ch',
-    url='https://github.com/JHayoz/SpyFFIER',
-    project_urls={},
     packages=['spyffier'],
-    package_data={'spyffier': ['*.txt']},
-    install_requires=install_requires,
-    tests_require=[],
-    license='GNU',
-    zip_safe=False,
-    keywords='spyffier',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Astronomy',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.10',
-    ],
+    url='https://github.com/JHayoz/SpyFFIER',
+    license='MIT',
+    author='Jean Hayoz',
+    author_email='jeanhayoz94@gmail.com',
+    description='Python package to reduce VLT/ERIS/SPIFFIER data using esorex commands and custom functionalities.',
+    install_requires=[
+                'numpy',
+                'scipy',
+                'matplotlib',
+                'astropy',
+                'PyAstronomy',
+                'pandas',
+                'sklearn',
+                'seaborn',
+                'spectres',
+                'photutils',
+                'skycalc_ipy'],
 )
